50fedbdd6f73a56ab48a6b4b92de99086c1f9766
Ricky Lee <rickyleeworks2@gmail.com>
2024-01-12 17:37:34 -0800

    project card column change; now accepts html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,7 @@ export default function Page() {
 
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
+          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-1 lg:grid-cols-1">
             {RESUME_DATA.projects.map((project) => {
               return (
                 <ProjectCard
